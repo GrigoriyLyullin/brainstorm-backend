0f7ae48e60735faebe2d4cbd26da55db02d26934
Grigorii Liullin <grigorii.liullin@t-systems.com>
2019-07-29 13:09:03 +0300

    Implement base

---
+++ src/main/java/com/brainstorm/BrainstormApplication.java
@@ -0,0 +1,13 @@
+package com.brainstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainstormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainstormApplication.class, args);
+    }
+
+}
