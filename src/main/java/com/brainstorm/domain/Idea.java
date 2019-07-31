5c1f340b5f6228c465399dc1591a9412f93df19a
Grigorii Liullin <grigorii.liullin@t-systems.com>
2019-07-31 16:53:11 +0300

    Disable CORS, add created and updated fields for Idea entity

--- src/main/java/com/brainstorm/domain/Idea.java
+++ src/main/java/com/brainstorm/domain/Idea.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.Date;
 import java.util.List;
 
 @Document(collection = "idea")
@@ -26,16 +29,22 @@ public class Idea {
     @Field(value = "description")
     private String description;
 
-    @DBRef
-    private List<Category> categories;
+    @Field(value = "category")
+    private String category;
+
+    @CreatedDate
+    private Date createdDate;
+
+    @LastModifiedDate
+    private Date lastModifiedDate;
 
     @DBRef
     private List<Voter> voters;
 
-    public Idea(String title, String description, List<Category> categories, List<Voter> voters) {
-        this.title = title;
-        this.description = description;
-        this.categories = categories;
-        this.voters = voters;
-    }
+//    public Idea(String title, String description, Category category, List<Voter> voters) {
+//        this.title = title;
+//        this.description = description;
+//        this.category = category;
+//        this.voters = voters;
+//    }
 }
